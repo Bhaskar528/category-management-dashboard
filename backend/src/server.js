77a1c6ec510ce25dc4ead15a90e92e46e7846564
bhaskar <bhaskarc2468@gmail.com>
2026-01-12 00:55:45 +0530

    updated url in []

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-    origin:"https://category-management-dashboard-jyz6.vercel.app/"
+    origin:["https://category-management-dashboard-jyz6.vercel.app/"]
 }));
 app.use(express.json());
 app.use("/uploads", express.static("src/uploads"));
